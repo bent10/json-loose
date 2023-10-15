41882afe633f22d2e0a8b9fa2ecd4e621b535593
bent10 <bent10@stilearning.com>
2023-10-15 12:24:53 +0700

    build: tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    minify: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'jsonLoose',
